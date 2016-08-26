975bacd42305d3d84a5056659b1b8b7d2be484a8
Andrew Chen <chenan02@gmail.com>
2016-08-26 01:39:30 -0400

    Seperate javascript, delete comments, make animation faster

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function (){
+	$("#about").click(function (){
+		$('html, body').animate({
+			scrollTop: $(".background2").offset().top
+	   }, 1500);
+	});
+	$("#projects").click(function (){
+		$("html, body").animate({
+		  scrollTop: $(".background3").offset().top
+		}, 1500);
+	});
+	$("#contact").click(function(){
+		$("html, body").animate({
+			scrollTop: $(".background4").offset().top
+		}, 1500);
+	});
+});
